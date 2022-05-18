aca95223b56e463b1cbf39f0be235af442f0e8f2
Adrien Gentil <agentil@redhat.com>
2022-05-18 14:27:01 +0200

    Add CI script to publish docker images (#2)
    New script to be picked up by the CI and publish the `latest` image and
    a version tagged with the commit SHA of the repo.

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+CONTAINER_CMD := $(or $(CONTAINER_CMD), $(shell command -v podman 2> /dev/null))
+ifndef CONTAINER_CMD
+CONTAINER_CMD := docker
+endif
+
+PROW_JOBS_SCRAPER_IMAGE := $(or $(PROW_JOBS_SCRAPER_IMAGE),quay.io/edge-infrastructure/prow-jobs-scraper)
+PROW_JOBS_SCRAPER_TAG := $(or $(PROW_JOBS_SCRAPER_TAG),latest)
+
 install:
 	pip install .
 
@@ -22,4 +30,7 @@ mypy:
 lint: mypy format
 	git diff --exit-code
 
-.PHONY: install install-lint install-unit-tests full-install unit-tests format mypy lint
+build-image:
+	$(CONTAINER_CMD) build $(CONTAINER_BUILD_EXTRA_PARAMS) -t $(PROW_JOBS_SCRAPER_IMAGE):$(PROW_JOBS_SCRAPER_TAG) .
+
+.PHONY: install install-lint install-unit-tests full-install unit-tests format mypy lint build-image
---
+++ build_deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+if command -v podman
+then
+    CONTAINER_CMD="podman"
+else
+    CONTAINER_CMD="docker"
+fi
+export CONTAINER_CMD
+echo "Using ${CONTAINER_CMD} as container engine..."
+
+export CONTAINER_BUILD_EXTRA_PARAMS=${CONTAINER_BUILD_EXTRA_PARAMS:-"--no-cache"}
+export PROW_JOBS_SCRAPER_IMAGE=${PROW_JOBS_SCRAPER_IMAGE:-"quay.io/app-sre/prow-jobs-scraper"}
+
+# Tag with the current commit sha
+PROW_JOBS_SCRAPER_TAG="$(git rev-parse HEAD)"
+export PROW_JOBS_SCRAPER_TAG
+
+# Setup credentials to image registry
+${CONTAINER_CMD} login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
+
+# Build and push latest image
+make build-image
+
+PROW_JOBS_SCRAPER_IMAGE_COMMIT_SHA="${PROW_JOBS_SCRAPER_IMAGE}:${PROW_JOBS_SCRAPER_TAG}"
+${CONTAINER_CMD} push "${PROW_JOBS_SCRAPER_IMAGE_COMMIT_SHA}"
+
+# Tag the image as latest
+PROW_JOBS_SCRAPER_IMAGE_LATEST="${PROW_JOBS_SCRAPER_IMAGE}:latest"
+${CONTAINER_CMD} tag "${PROW_JOBS_SCRAPER_IMAGE_COMMIT_SHA}" "${PROW_JOBS_SCRAPER_IMAGE_LATEST}"
+${CONTAINER_CMD} push "${PROW_JOBS_SCRAPER_IMAGE_COMMIT_SHA}"
