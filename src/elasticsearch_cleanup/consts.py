3e9ff141a69f1547b6c64e6a22b3f46718adce21
Dan Manor <dmanor@redhat.com>
2023-10-11 15:02:09 +0300

    NO-ISSUE: Sort documents by id length in ascending order (#255)

--- src/elasticsearch_cleanup/consts.py
+++ src/elasticsearch_cleanup/consts.py
@@ -1,5 +1,14 @@
 from typing import Any, Final
 
 OPENSEARCH_QUERY_ALL_INDEX_DOCUMENTS: Final[dict[str, Any]] = {
-    "query": {"match_all": {}}
+    "query": {"match_all": {}},
+    "sort": [
+        {
+            "_script": {
+                "type": "number",
+                "script": {"source": "doc['_id'].value.length()", "lang": "painless"},
+                "order": "asc",
+            }
+        }
+    ],
 }
